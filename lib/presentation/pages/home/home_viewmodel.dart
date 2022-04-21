a8eb158a20b87e683c86b09c711885c085c66252
Carlos Diaz <carlos.cedp@gmail.com>
2022-04-21 16:15:02 -0400

    ✨ add api call to viewModel

--- lib/presentation/pages/home/home_viewmodel.dart
+++ lib/presentation/pages/home/home_viewmodel.dart
@@ -1,9 +1,26 @@
-import 'package:flutter_template_clean_architecture/domain/usecases/get_employee_by_code.dart';
+import 'package:flutter_template_clean_architecture/domain/entities/employee.dart';
+import 'package:flutter_template_clean_architecture/domain/usecases/get_employee_by_code.dart'
+    as get_employee_by_code;
 import 'package:stacked/stacked.dart';
 
 class HomeViewModel extends BaseViewModel {
-  final GetEmployeeByCode getEmployeeByCode;
+  final get_employee_by_code.GetEmployeeByCode getEmployeeByCode;
+  Employee? _employee;
+  Employee? get employee => _employee;
   HomeViewModel({
     required this.getEmployeeByCode,
   });
+
+  Future<void> callApi() async {
+    clearErrors();
+    setBusyForObject(_employee, true);
+    var useCaseParams = get_employee_by_code.Params(code: '1234');
+    var response = await getEmployeeByCode.execute(useCaseParams);
+    response.fold((ex) {
+      setError(ex);
+    }, (data) {
+      _employee = data;
+    });
+    setBusyForObject(_employee, false);
+  }
 }
